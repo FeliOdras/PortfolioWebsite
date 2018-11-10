a96d445ae0415114917871c7d0cb37b1be837e67
FeliOdras <ariane.feldmann@odras.de>
2018-11-10 15:18:32 +0100

    Beginning of header and navigation moved to external files

---
+++ includes/headerFront.php
@@ -0,0 +1,6 @@
+
+<!-- I have added a class to body here so I can address elements that need a special style only on the frontpage (for example the header here)-->
+<body class="frontpage">
+    <!-- This container wraps the whole content -->
+    <div id="main-container">
+        <?php include 'navigation.php'; ?>
