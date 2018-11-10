89bd07109a36dac59d5acb68442d66ec4bb423bb
FeliOdras <ariane.feldmann@odras.de>
2018-11-10 15:37:58 +0100

    About Section from frontpage moved to external file

--- index.php
+++ index.php
@@ -4,33 +4,11 @@
     $pageTitle	= 'Make code <em>Beautiful</em>';
 
     include 'includes/head.php';
-    include 'includes/header/headerFront.php'; ?>
+    include 'includes/header/headerFront.php'; 
+    include 'includes/content/frontpage/about.php'; 
+?>
         
-            <!-- Start of Section 'About me' -->
-            <a id="about"></a>
-            <section id="home_me" class="inverse">
-                <!-- bookmark link for "About"-Section -->
-                <!-- The main_wrapper holds the content to a maximum width and keeps it in the middle of the screen -->
-                <div class="main_wrapper">
-                    <!-- article is displayed in a box -->
-                    <h3 class="sectionheading">How it all began</h3>
-                    <article class="box boxright">
-                        <h4>History</h4>
-                        <figure class="imgright">
-                            <a href="animatedCV/" target="_blank"><img src="images/animatedCV.png" width="300" alt="animated CV"></a>
-                            <figcaption>Open animated CV</figcaption>
-                        </figure>
-                        <p>It was in 1999 when I "built" my first website. It was kind of crappy, built with Microsoft FrontPage, one of the first WYSIWYG desktop editors of its kind, and naturally it only worked in Internet Explorer. A few years later I came to a place called "c-base" (the people who run this place pretend to reconstruct a crashed space station, some of them might really believe it) and there I met some cool guys who really knew how to build a website. One of them showed me the German HTML reference project <a href="http://www.selfhtml.org" target="_blank">selfhtml.org</a>. 'If you want to build a website, read this', he said. So I did and so I fell in love with code.</p>
-                        <hr class="clr">
-                        <a class="button boxleft aligncenter" href="about.html"><em>Got curious?</em> <br>Find out more &raquo;</a>
-                        <!-- clear floated content -->
-                        <hr class="clr">
-                    </article>
-                    <!-- End of boxed and right aligned (.boxright) article -->
-                </div>
-                <!-- End of Main Wrapper -->
-            </section>
-            <!-- End of Section 'About' -->
+            
             <!-- Start of Section 'Skills' -->
             <a id="skills"></a>
             <section id="home_skills">
