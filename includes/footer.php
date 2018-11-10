93c56f625700d784642f5c54ad0741d6af9b8ebc
FeliOdras <ariane.feldmann@odras.de>
2018-11-10 16:02:52 +0100

    Link to DCI changed

--- includes/footer.php
+++ includes/footer.php
@@ -11,7 +11,7 @@
         <!-- End of Footer Section -->
         <nav id="meta-menu">
             <ul>
-                <li><a href="dci.html" title="Working Materials for the courses at Digital Career Institute">DCI</a></li>
+                <li><a href="http://www.digitalcareerinstitute.org" title="Digital Career Institute">DCI</a></li>
                 <li><a href="contact.html">Contact</a></li>
                 <li><a href="disclaimer.html">Imprint and Disclaimer</a></li>
             </ul>
@@ -20,4 +20,5 @@
             <a href="http://validator.w3.org/check/referer" target="_blank" title="Validate MarkUp">Valid HTML5 <i class="fa fa-html5"></i></a> | <a href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fme.odras.de%2Fstyle%2Fstyle.css" title="Validate Stylesheet" target="_blank" rel="nofollow">Valid CSS3 <i class="fa fa-css3"></i></a></div>
         </div>
 </body>
+
 </html>
