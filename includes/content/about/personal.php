bce2d9426416301b2ecaf9f3c5ef32fb24c242f0
FeliOdras <ariane.feldmann@odras.de>
2018-11-10 17:06:53 +0100

    Content sections and footer of about page moved to external files

---
+++ includes/content/about/personal.php
@@ -0,0 +1,19 @@
+<a id="personal"></a>
+            <section id="about_personal">
+                <div class="section_wrapper">
+                    <h3 class="sectionheading alignright">Personal</h3>
+                    <article class="box boxleft">
+                        <h4>Aikido</h4>
+                        <p>Around my 40th birthday I discovered Aikido for me. Aikido belongs to the eastern martial arts. It origins in Japan and is considered the most <strong>peaceful</strong> kind of martial arts. There are no attack techniques and no competitions.</p>
+                        <p>Aikido is something where you don't act against each other but <strong>together</strong>. It also focusses on the <strong>inner balance</strong> of mind, soul and body.</p>
+                        <hr>
+                        <h4>c-base</h4>
+                        <p><a href="https://de.wikipedia.org/wiki/C-base" target="_blank" class="ext">c-base</a> is known to many as the "mother of <strong>hackerspaces</strong>", but actually this is only half the truth. c-base has a hacker space but it is much more than this. Its crew members pretend to reconstruct a <strong>crashed space station</strong>, some might even believe this to be true, at least on a philosophical level.</p>
+                        <p>I have been a member of c-base <strong>since 2002</strong>. I am deeply connected to this place since I've met a lot of inspiring and helpful people here who supported my in discovering and developing my passion for <strong>coding</strong>.</p>
+                        <hr>
+                        <h4>fantasy blogger</h4>
+                        <p>I've always been someone who loves to dive into imaginative worlds, so it is not surprising that I got excited about <strong>pen &amp; paper roleplaying games</strong> when I learned about it. In my twenties I tried out a lot of those.</p>
+                        <p>Today there is just one group left, but this is of a <strong>special quality</strong>. This group already exists for ten years. We meet every 4 to 6 weeks to travel on the ways of our <strong>imagination</strong> and for four years now we have a blog where we <strong>write down our story</strong> out of the sight of our character. I have started to review and revise my own entries and to publish them on my <a href="http://www.odras.de/" target="_blank" class="ext">website</a> and as e-book.</p>
+                    </article>
+                </div>
+            </section>
