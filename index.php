51627cfa1024c7ed2f55b477f6870d45e94fbfa9
FeliOdras <ariane.feldmann@odras.de>
2018-11-10 15:52:11 +0100

    Moved footer section on index to external file

--- index.php
+++ index.php
@@ -7,29 +7,6 @@
     include 'includes/header/headerFront.php'; 
     include 'includes/content/frontpage/about.php';
     include 'includes/content/frontpage/skills.php';     
-    include 'includes/content/frontpage/projects.php'; 
+    include 'includes/content/frontpage/projects.php';
+    include 'includes/footer.php';
 ?>
-        </main>
-        <!-- End of Main Section -->
-        <!-- Start of Footer Section -->
-        <footer id="main-footer">
-            <ul id="social-navigation">
-                <li><a href="https://www.xing.com/profile/Ariane_Feldmann" target="_blank"><i class="fa fa-xing"></i></a></li>
-                <li><a href="https://de.linkedin.com/in/ariane-feldmann" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-                <li><a href="https://github.com/FeliOdras?tab=repositories" target="_blank"><i class="fa fa-github"></i></a></li>
-            </ul>
-        </footer>
-        <!-- End of Footer Section -->
-        <nav id="meta-menu">
-            <ul>
-                <li><a href="dci.html" title="Working Materials for the courses at Digital Career Institute">DCI</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="disclaimer.html">Imprint and Disclaimer</a></li>
-            </ul>
-        </nav>
-        <div class="validation">
-            <a href="http://validator.w3.org/check/referer" target="_blank" title="Validate MarkUp">Valid HTML5 <i class="fa fa-html5"></i></a> | <a href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fme.odras.de%2Fstyle%2Fstyle.css" title="Validate Stylesheet" target="_blank" rel="nofollow">Valid CSS3 <i class="fa fa-css3"></i></a></div>
-        </div>
-</body>
-
-</html>
