8800242a5dccf8bdcd5f0af2b9b7c3159c0bef6a
FeliOdras <ariane.feldmann@odras.de>
2018-11-10 15:49:12 +0100

    Projects section on frontpage moved to external file

--- index.php
+++ index.php
@@ -6,49 +6,9 @@
     include 'includes/head.php';
     include 'includes/header/headerFront.php'; 
     include 'includes/content/frontpage/about.php';
-    include 'includes/content/frontpage/skills.php'; 
+    include 'includes/content/frontpage/skills.php';     
+    include 'includes/content/frontpage/projects.php'; 
 ?>
-        
-            
-            
-            <!-- Start of Section 'Code and Project' -->
-            <a id="projects"></a>
-            <section id="home_code" class="inverse">
-                <div class="main_wrapper">
-                    <h3 class="sectionheading alignleft">Code and Projects</h3>
-                    <div class="flexbox">
-                        <div class="flexboxitem box">
-                            <a href="projects.html#html">
-                                <div class="icons">
-                                    <i class="fa fa-html5"></i>
-                                    <i class="fa fa-css3"></i>
-                                </div>
-                                <h4>HTML and CSS</h4>
-                                <p>Code examples for some fancy effects with CSS3.</p>
-                            </a>
-                        </div>
-                        <div class="flexboxitem box">
-                            <a href="projects.html#themes">
-                                <div class="icons"><i class="fa fa-wordpress"></i>
-                                    <i class="fa fa-joomla"></i>
-                                </div>
-                                <h4>Themes and Templates for CMS</h4>
-                                <p>Individually styled WordPress sites</p>
-                            </a>
-                        </div>
-                        <div class="flexboxitem box">
-                            <a href="projects.html#javascript">
-                                <div class="icons">
-                                    <i class="fa fa-code"></i>
-                                </div>
-                                <h4>JavaScript and its Frameworks</h4>
-                                <p>Coming soon...</p>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- End of Section 'Code and Projects' -->
         </main>
         <!-- End of Main Section -->
         <!-- Start of Footer Section -->
