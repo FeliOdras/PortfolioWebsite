9c17a0ff5274deac32cad2f232c0ff6b52a17b19
FeliOdras <ariane.feldmann@odras.de>
2018-12-04 13:12:41 +0100

    Created projects.php

---
+++ projects.php
@@ -0,0 +1,14 @@
+<?php 
+    $pageID	    = 'skills';
+    $headTitle	= 'Skills and capabilities | Ariane Feldmann';
+    $pageTitle	= '<em>Talent</em> is just great <em>Patience</em>';
+
+    include 'includes/head.php';
+    include 'includes/header/headerContent.php'; 
+    include 'includes/content/skills/coding.php';
+    include 'includes/content/skills/cms.php';     
+    include 'includes/content/skills/grafic.php';
+    include 'includes/content/skills/languages.php';
+    include 'includes/content/skills/legend.php';
+    include 'includes/footer.php';
+?>
