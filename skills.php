4d7c4827eef85847c3e7aceebaf8187c0a9e293d
FeliOdras <ariane.feldmann@odras.de>
2018-12-04 12:55:52 +0100

    Initialized structure for abot site

---
+++ skills.php
@@ -0,0 +1,12 @@
+<?php 
+    $pageID	    = 'index';
+    $headTitle	= 'Ariane Feldmann - Frontend.Developer by passion';
+    $pageTitle	= 'Make code <em>Beautiful</em>';
+
+    include 'includes/head.php';
+    include 'includes/header/headerFront.php'; 
+    include 'includes/content/frontpage/about.php';
+    include 'includes/content/frontpage/skills.php';     
+    include 'includes/content/frontpage/projects.php';
+    include 'includes/footer.php';
+?>
