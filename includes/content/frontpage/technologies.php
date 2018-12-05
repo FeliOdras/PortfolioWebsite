c92d1d7850ab38147b5d0291e65f138d4fb4e617
FeliOdras <ariane.feldmann@odras.de>
2018-12-05 09:56:20 +0100

    Added Ocons to frontpage section technologies

--- includes/content/frontpage/technologies.php
+++ includes/content/frontpage/technologies.php
@@ -1,17 +1,12 @@
 <!-- Start of Section 'About me' -->
         <a id="about"></a>
-            <section id="home_me" class="inverse">
+            <section id="home_tech" class="inverse">
                 <!-- bookmark link for "About"-Section -->
                 <!-- The main_wrapper holds the content to a maximum width and keeps it in the middle of the screen -->
                 <div class="main_wrapper">
                     <!-- article is displayed in a box -->
-                    <h3 class="sectionheading">How it all began</h3>
-                    <article class="box boxright">
-                        <h4>History</h4>
-                        <p>It was in 1999 when I "built" my first website. It was kind of crappy, built with Microsoft FrontPage, one of the first WYSIWYG desktop editors of its kind, and naturally it only worked in Internet Explorer. A few years later I came to a place called "c-base" (the people who run this place pretend to reconstruct a crashed space station, some of them might really believe it) and there I met some cool guys who really knew how to build a website. One of them showed me the German HTML reference project <a href="http://www.selfhtml.org" target="_blank">selfhtml.org</a>. 'If you want to build a website, read this', he said. So I did and so I fell in love with code.</p>
-                        <a class="button boxleft aligncenter" href="about.php"><em>Got curious?</em> <br>Find out more &raquo;</a>
-                        <!-- clear floated content -->
-                        <hr class="clr">
+                    <article class="aligncenter">
+                    <i class="fab fa-html5"></i> <i class="fab fa-css3-alt"></i> <i class="fab fa-sass"></i> <i class="fab fa-js"></i> <i class="fab fa-font-awesome-alt"></i> <i class="fab fa-wordpress-simple"></i> <i class="fab fa-git"></i> 
                     </article>
                     <!-- End of boxed and right aligned (.boxright) article -->
                 </div>
