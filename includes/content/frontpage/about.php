a70c789a7c9ced5bfae50614559fb699dfb16469
FeliOdras <ariane.feldmann@odras.de>
2018-12-05 10:02:11 +0100

    Alignment adjustments on front page

--- includes/content/frontpage/about.php
+++ includes/content/frontpage/about.php
@@ -5,8 +5,8 @@
                 <!-- The main_wrapper holds the content to a maximum width and keeps it in the middle of the screen -->
                 <div class="main_wrapper">
                     <!-- article is displayed in a box -->
-                    <h3 class="sectionheading alignright">How it all began</h3>
-                    <article class="box boxleft">
+                    <h3 class="sectionheading">How it all began</h3>
+                    <article class="box boxright">
                         <h4 class="">History</h4>
                         <p>It was in 1999 when I "built" my first website. It was kind of crappy, built with Microsoft FrontPage, one of the first WYSIWYG desktop editors of its kind, and naturally it only worked in Internet Explorer. A few years later I came to a place called "c-base" (the people who run this place pretend to reconstruct a crashed space station, some of them might really believe it) and there I met some cool guys who really knew how to build a website. One of them showed me the German HTML reference project <a href="http://www.selfhtml.org" target="_blank">selfhtml.org</a>. 'If you want to build a website, read this', he said. So I did and so I fell in love with code.</p>
                         <a class="button boxleft aligncenter" href="about.php"><em>Got curious?</em> <br>Find out more &raquo;</a>
