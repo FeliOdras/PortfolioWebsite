da7513c024a32af7491c22bee9d3981b36c6bcbe
FeliOdras <ariane.feldmann@odras.de>
2018-12-10 09:11:10 +0100

    Added tech icons to header on Frontpage

---
+++ includes/header/headerTechs.php
@@ -0,0 +1,7 @@
+                <div class="tech_wrapper">
+                    <!-- article is displayed in a box -->
+                    <h2 class="alignright">
+                    <i class="fab fa-html5"></i> <i class="fab fa-css3-alt"></i> <i class="fab fa-sass"></i> <i class="fab fa-js"></i> <i class="fab fa-font-awesome-alt"></i> <i class="fab fa-wordpress-simple"></i> <i class="fab fa-git"></i> 
+                    </h2>
+                    <!-- End of boxed and right aligned (.boxright) article -->
+                </div>
