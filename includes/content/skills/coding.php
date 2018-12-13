32881d70f36a57fa4637dd14cf2a6ba5fc86e4bf
FeliOdras <ariane.feldmann@odras.de>
2018-12-13 13:58:35 +0100

    Minor adjustments in menu + on skills page

--- includes/content/skills/coding.php
+++ includes/content/skills/coding.php
@@ -7,7 +7,7 @@
                         <ul class="skillList">
                             <li>HTML/HTML5 <i class="fas fa-certificate"></i></li>
                             <li>CSS/CSS3 <i class="fas fa-certificate"></i></li>
-                            <li>Responsive Webdesign <i class="fa fa-certificate"></i></li>
+                            <li>Responsive Webdesign <i class="fas fa-certificate"></i></li>
                             <li>jQuery <i class="fas fa-star"></i></li>
                             <li>PHP <i class="fas fa-star-half-alt"></i></li>
                         </ul>
