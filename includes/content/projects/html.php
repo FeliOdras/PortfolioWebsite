c3c40b00d6c044d7248f2691d794dce31e9fee8e
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 11:32:10 +0100

    Added class 'hidden' to projects h4

--- includes/content/projects/html.php
+++ includes/content/projects/html.php
@@ -4,12 +4,12 @@
                     <h3 class="sectionheading alignright">Pure HTML and CSS</h3>
                     <div class="flexbox">
                         <article class="box flexboxitem">
-                            <h4>Portfolio Website</h4>
+                            <h4 class="hidden">Portfolio Website</h4>
                             <a href="https://me.odras.de" target="_blank">
                             <img src="images/html-portfolio.png" alt="Portfolio Website"></a>                            
                         </article>
                         <article class="box flexboxitem">
-                            <h4>Animated CV</h4>
+                            <h4 class="hidden">Animated CV</h4>
                             <a href="https://me.odras.de//animatedCV/" target="_blank">
                             <img src="images/animatedCV.png" alt="Animated CV"></a>                            
                         </article>                        
--- includes/content/projects/wordpress.php
+++ includes/content/projects/wordpress.php
@@ -4,17 +4,17 @@
                     <h3 class="sectionheading alignright">WordPress</h3>
                     <div class="flexbox">
                         <article class="box flexboxitem">
-                            <h4>CoCo Contact - Contact + Communication</h4>
+                            <h4 class="hidden">CoCo Contact - Contact + Communication</h4>
                             <a href="https://www.coco-contact.de" target="_blank">
                             <img src="images/wordpress-coco.png" alt="CoCo - Contact + Communication"></a>                            
                         </article>
                         <article class="box flexboxitem">
-                            <h4>Brennstoffhandel Habermann</h4>
+                            <h4 class="hidden">Brennstoffhandel Habermann</h4>
                             <a href="https://www.heizoel-habermann.de" target="_blank">
                             <img src="images/wordpress-habermann.png" alt="Brennstoffhandel Habermann"></a>                            
                         </article>                        
                         <article class="box flexboxitem">
-                            <h4>Education Sports</h4>
+                            <h4 class="hidden">Education Sports</h4>
                             <a href="https://www.educationsports.de" target="_blank">
                             <img src="images/wordpress-education-sports.png" alt="Education Sports"></a>                            
                         </article>
