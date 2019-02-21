4cb63d6988ffe2d497a219cdea693f61e0b9de47
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 09:48:29 +0100

    Added Example Box to JS section in projects

--- includes/content/projects/javascript.php
+++ includes/content/projects/javascript.php
@@ -2,9 +2,12 @@
             <section id="code_javascript" class="inverse">
                 <div class="section_wrapper">
                     <h3 class="sectionheading alignleft">JavaScript</h3>
-                        <article class="box boxright">
-                            <h4>JavaScript and Frameworks</h4>
-                            Coming soon...
-                        </article>
+                        <div class="flexbox">
+                            <article class="box flexboxitem">
+                                <h4>Portfolio Website</h4>
+                                <a href="https://me.odras.de" target="_blank">
+                                <img src="images/html-portfolio.png" alt="Portfolio Website"></a>                            
+                            </article>
+                        </div>
                 </div>
             </section>
