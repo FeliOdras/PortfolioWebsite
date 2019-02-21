646b2afc920426eaf4f4ff33cbafe77cd7c7bc4e
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 11:47:30 +0100

    Wrapped link around h4 in Projects html section

--- includes/content/projects/html.php
+++ includes/content/projects/html.php
@@ -4,12 +4,12 @@
                     <h3 class="sectionheading alignright">Pure HTML and CSS</h3>
                     <div class="flexbox">
                         <article class="box flexboxitem">
-                            <h4 class="hidden">Portfolio Website</h4>
+                            <h4 class="hidden"><a href="https://me.odras.de" target="_blank">Portfolio Website</a></h4>
                             <a href="https://me.odras.de" target="_blank">
                             <img src="images/html-portfolio.png" alt="Portfolio Website"></a>                            
                         </article>
                         <article class="box flexboxitem">
-                            <h4 class="hidden">Animated CV</h4>
+                            <h4 class="hidden"><a href="https://me.odras.de//animatedCV/" target="_blank">Animated CV</a></h4>
                             <a href="https://me.odras.de//animatedCV/" target="_blank">
                             <img src="images/animatedCV.png" alt="Animated CV"></a>                            
                         </article>                        
