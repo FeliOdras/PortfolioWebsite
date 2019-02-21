800dfd73b179fdd91f699a526e65f3ef51b14244
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 11:30:39 +0100

    Adjusted flexbox item width

--- includes/content/projects/javascript.php
+++ includes/content/projects/javascript.php
@@ -4,37 +4,37 @@
                     <h3 class="sectionheading alignleft">JavaScript</h3>
                         <div class="flexbox">
                             <article class="box flexboxitem">
-                                <h4>The Sounds of Silence</h4>
+                                <h4 class="hidden">The Sounds of Silence</h4>
                                 <a href="https://playground.odras.de/TheSoundsOfSilence/" target="_blank">
                                 <img src="images/theSoundsOfSilence.jpg" alt="The Sounds of Silence"></a>   
                                 <p>Music App with iTunes API</p>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4>Tip Calculator</h4>
+                                <h4 class="hidden">Tip Calculator</h4>
                                 <a href="https://playground.odras.de/TipCalculator/" target="_blank">
                                 <img src="images/tipCalculator.jpg" alt="Tip Calculator"></a>   
                                 <p>Small JS App to calculate tip</p>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4>Quote of the day</h4>
+                                <h4 class="hidden">Quote of the day</h4>
                                 <a href="https://playground.odras.de/QuoteOfTheDay/" target="_blank">
                                 <img src="images/quoteOfTheDay.jpg" alt="Quote of the day"></a>   
                                 <p>Random quote generator</p>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4>Math Game</h4>
+                                <h4 class="hidden">Math Game</h4>
                                 <a href="https://playground.odras.de/MathGame/" target="_blank">
                                 <img src="images/MathGame.jpg" alt="Math Game"></a>   
                                 <p>Practice basic calculation for 2nd grade elementary school children</p>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4>Fortune Teller</h4>
+                                <h4 class="hidden">Fortune Teller</h4>
                                 <a href="https://playground.odras.de/FortuneTellingGame/" target="_blank">
                                 <img src="images/FortuneTeller.jpg" alt="Fortune Teller"></a>   
                                 <p>Random fortune generator</p>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4>Animal Age Calculator</h4>
+                                <h4 class="hidden">Animal Age Calculator</h4>
                                 <a href="https://playground.odras.de/Javascript-Animal-Age-Calculator/" target="_blank">
                                 <img src="images/animalsAge.png" alt="Animal Age Calculator"></a>   
                                 <p>Calculate animal ages</p>                         
