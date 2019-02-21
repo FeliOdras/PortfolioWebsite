3545fdbce6c84c906fca19c7f39415caa640bd1d
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 11:50:53 +0100

    Wrapped link around h4 in Projects wp section

--- includes/content/projects/wordpress.php
+++ includes/content/projects/wordpress.php
@@ -4,17 +4,17 @@
                     <h3 class="sectionheading alignright">WordPress</h3>
                     <div class="flexbox">
                         <article class="box flexboxitem">
-                            <h4 class="hidden">CoCo Contact - Contact + Communication</h4>
+                            <h4 class="hidden"><a href="https://www.coco-contact.de" target="_blank">CoCo Contact - Contact + Communication</a></h4>
                             <a href="https://www.coco-contact.de" target="_blank">
                             <img src="images/wordpress-coco.png" alt="CoCo - Contact + Communication"></a>                            
                         </article>
                         <article class="box flexboxitem">
-                            <h4 class="hidden">Brennstoffhandel Habermann</h4>
+                            <h4 class="hidden"><a href="https://www.heizoel-habermann.de" target="_blank">Brennstoffhandel Habermann</a></h4>
                             <a href="https://www.heizoel-habermann.de" target="_blank">
                             <img src="images/wordpress-habermann.png" alt="Brennstoffhandel Habermann"></a>                            
                         </article>                        
                         <article class="box flexboxitem">
-                            <h4 class="hidden">Education Sports</h4>
+                            <h4 class="hidden"><a href="https://www.educationsports.de" target="_blank">Education Sports></a></h4>
                             <a href="https://www.educationsports.de" target="_blank">
                             <img src="images/wordpress-education-sports.png" alt="Education Sports"></a>                            
                         </article>
