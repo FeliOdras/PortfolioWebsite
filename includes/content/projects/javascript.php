e5a0e80911ca8f0fcfadecd2b1a6e064966c8b61
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 11:46:33 +0100

    Wrapped link around h4 in Projects

--- includes/content/projects/javascript.php
+++ includes/content/projects/javascript.php
@@ -4,42 +4,47 @@
                     <h3 class="sectionheading alignleft">JavaScript</h3>
                         <div class="flexbox">
                             <article class="box flexboxitem">
-                                <h4 class="hidden">The Sounds of Silence
-                                    <span>Music App with iTunes API</span>  
+                                <h4 class="hidden"><a href="https://playground.odras.de/TheSoundsOfSilence/" target="_blank">The Sounds of Silence
+                                    <span>Music App with iTunes API</span> </a> 
                                 </h4>
                                 <a href="https://playground.odras.de/TheSoundsOfSilence/" target="_blank">
                                 <img src="images/theSoundsOfSilence.jpg" alt="The Sounds of Silence"></a>   
                                                       
                             </article>
                             <article class="box flexboxitem">
-                                <h4 class="hidden">Tip Calculator</h4>
+                                <h4 class="hidden"><a href="https://playground.odras.de/TipCalculator/" target="_blank">Tip Calculator
+                                    <span>Small JS App to calculate tip</span></a>
+                                </h4>
                                 <a href="https://playground.odras.de/TipCalculator/" target="_blank">
-                                <img src="images/tipCalculator.jpg" alt="Tip Calculator"></a>   
-                                <p>Small JS App to calculate tip</p>                         
+                                <img src="images/tipCalculator.jpg" alt="Tip Calculator"></a>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4 class="hidden">Quote of the day</h4>
+                                <h4 class="hidden"><a href="https://playground.odras.de/QuoteOfTheDay/" target="_blank">Quote of the day
+                                    <span>Random quote generator</span></a>
+                                </h4>
                                 <a href="https://playground.odras.de/QuoteOfTheDay/" target="_blank">
-                                <img src="images/quoteOfTheDay.jpg" alt="Quote of the day"></a>   
-                                <p>Random quote generator</p>                         
+                                <img src="images/quoteOfTheDay.jpg" alt="Quote of the day"></a>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4 class="hidden">Math Game</h4>
+                                <h4 class="hidden"><a href="https://playground.odras.de/MathGame/" target="_blank">Math Game
+                                    <span>Practice basic calculation for 2nd grade elementary school children</span></a>
+                                </h4>
                                 <a href="https://playground.odras.de/MathGame/" target="_blank">
-                                <img src="images/MathGame.jpg" alt="Math Game"></a>   
-                                <p>Practice basic calculation for 2nd grade elementary school children</p>                         
+                                <img src="images/MathGame.jpg" alt="Math Game"></a>                          
                             </article>
                             <article class="box flexboxitem">
-                                <h4 class="hidden">Fortune Teller</h4>
+                                <h4 class="hidden"><a href="https://playground.odras.de/FortuneTellingGame/" target="_blank">Fortune Teller
+                                    <span>Random fortune generator</span></a>
+                                </h4>
                                 <a href="https://playground.odras.de/FortuneTellingGame/" target="_blank">
                                 <img src="images/FortuneTeller.jpg" alt="Fortune Teller"></a>   
-                                <p>Random fortune generator</p>                         
                             </article>
                             <article class="box flexboxitem">
-                                <h4 class="hidden">Animal Age Calculator</h4>
+                                <h4 class="hidden"><a href="https://playground.odras.de/Javascript-Animal-Age-Calculator/" target="_blank">Animal Age Calculator
+                                    <span>Calculate animal ages</span></a>
+                                </h4>
                                 <a href="https://playground.odras.de/Javascript-Animal-Age-Calculator/" target="_blank">
-                                <img src="images/animalsAge.png" alt="Animal Age Calculator"></a>   
-                                <p>Calculate animal ages</p>                         
+                                <img src="images/animalsAge.png" alt="Animal Age Calculator"></a>                          
                             </article>
                         </div>
                 </div>
