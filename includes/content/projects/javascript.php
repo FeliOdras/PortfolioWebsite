a39608b88aef0a6dbf9d5d80f532ff2b21f7982e
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 09:55:47 +0100

    Added images for JS Projects, changed content of first js projects box

--- includes/content/projects/javascript.php
+++ includes/content/projects/javascript.php
@@ -4,9 +4,10 @@
                     <h3 class="sectionheading alignleft">JavaScript</h3>
                         <div class="flexbox">
                             <article class="box flexboxitem">
-                                <h4>Portfolio Website</h4>
-                                <a href="https://me.odras.de" target="_blank">
-                                <img src="images/html-portfolio.png" alt="Portfolio Website"></a>                            
+                                <h4>The Sounds of Silence</h4>
+                                <a href="https://playground.odras.de/TheSoundsOfSilence/" target="_blank">
+                                <img src="images/theSoundsOfSilence.jpg" alt="The Sounds of Silence"></a>   
+                                <p>Music App with iTunes API</p>                         
                             </article>
                         </div>
                 </div>
