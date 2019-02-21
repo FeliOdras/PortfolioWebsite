74e7d97acc8287edcf8e4f615192a712403b33aa
FeliOdras <ariane.feldmann@odras.de>
2019-02-21 11:02:56 +0100

    Added more js references

--- includes/content/projects/javascript.php
+++ includes/content/projects/javascript.php
@@ -9,6 +9,36 @@
                                 <img src="images/theSoundsOfSilence.jpg" alt="The Sounds of Silence"></a>   
                                 <p>Music App with iTunes API</p>                         
                             </article>
+                            <article class="box flexboxitem">
+                                <h4>Tip Calculator</h4>
+                                <a href="https://playground.odras.de/TipCalculator/" target="_blank">
+                                <img src="images/tipCalculator.jpg" alt="Tip Calculator"></a>   
+                                <p>Small JS App to calculate tip</p>                         
+                            </article>
+                            <article class="box flexboxitem">
+                                <h4>Quote of the day</h4>
+                                <a href="https://playground.odras.de/QuoteOfTheDay/" target="_blank">
+                                <img src="images/quoteOfTheDay.jpg" alt="Quote of the day"></a>   
+                                <p>Random quote generator</p>                         
+                            </article>
+                            <article class="box flexboxitem">
+                                <h4>Math Game</h4>
+                                <a href="https://playground.odras.de/MathGame/" target="_blank">
+                                <img src="images/MathGame.jpg" alt="Math Game"></a>   
+                                <p>Practice basic calculation for 2nd grade elementary school children</p>                         
+                            </article>
+                            <article class="box flexboxitem">
+                                <h4>Fortune Teller</h4>
+                                <a href="https://playground.odras.de/FortuneTellingGame/" target="_blank">
+                                <img src="images/FortuneTeller.jpg" alt="Fortune Teller"></a>   
+                                <p>Random fortune generator</p>                         
+                            </article>
+                            <article class="box flexboxitem">
+                                <h4>Animal Age Calculator</h4>
+                                <a href="https://playground.odras.de/Javascript-Animal-Age-Calculator/" target="_blank">
+                                <img src="images/animalsAge.png" alt="Animal Age Calculator"></a>   
+                                <p>Calculate animal ages</p>                         
+                            </article>
                         </div>
                 </div>
             </section>
