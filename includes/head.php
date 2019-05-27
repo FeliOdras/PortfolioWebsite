f0bed3d11f54e56d2add9e2fe1b87b86f06721fb
odras <ariane.feldmann@odras.de>
2019-05-27 20:45:29 +0200

    Removed comments from head

--- includes/head.php
+++ includes/head.php
@@ -1,13 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
     <title><?php echo $headTitle; ?></title>
-    <!-- define your character set -->
     <meta charset="UTF-8">
-    <!-- this makes sure your Website is scaled to viewport width (viewport=section of a window where the web contents are displayed)-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- link to stylesheets -->
-    <!-- Font Awesome 4.7 => https://fontawesome.com/v4.7.0/ -->
     <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <!-- My custom Stylesheet -->
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
