dddeb176ae1fa4fa68076f84c1b2acf541d1f732
odras <ariane.feldmann@odras.de>
2019-05-31 00:58:32 +0200

    Added headline to skills section on frontpage

--- includes/content/frontpage/skills.php
+++ includes/content/frontpage/skills.php
@@ -2,6 +2,7 @@
 <!-- Start of Section 'Skills' -->
     <a id="skills"></a>
             <section id="home_skills" class="">
+            <h3 class="sectionheading aligncenter">Skills</h3>
                 <div class="flexbox">
                     <i class="fab fa-js-square"></i> <i class="fab fa-html5"></i> <i class="fab fa-css3-alt"></i> <i class="fab fa-react"></i> <i class="fab fa-git"></i> <i class="fab fa-adobe"></i> <i class="fab fa-wordpress"></i> <i class="fab fa-sass"></i> <i class="fab fa-bootstrap"></i> <i class="fab fa-npm"></i> <i class="fab fa-ubuntu"></i> <i class="fab fa-php"></i>
                 </div>
