b293749dc514e042c531f4be4bc14e1aed6ebff4
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 11:18:16 +0200

    Cebtered see more button on projects

--- includes/content/frontpage/projects.php
+++ includes/content/frontpage/projects.php
@@ -1,9 +1,9 @@
            <!-- Start of Section 'Code and Project' -->
            <a id="projects"></a>
-            <section id="home_code" class="inverse">
-                <h3 class="sectionheading aligncenter">Code and Projects</h3>
-                <article class="flexbox">
-                <div id="projects-list-frontpage" class="flexbox"></div> 
-                <a class="button boxcenter aligncenter btn-about" href="projects.php#code_javascript">See more...</a>
-            </section>
-            <!-- End of Section 'Code and Projects' -->
+           <section id="home_code" class="inverse">
+               <h3 class="sectionheading aligncenter">Code and Projects</h3>
+               <article>
+                   <div id="projects-list-frontpage" class="flexbox"></div>
+                   <p class="aligncenter"> <a class="button btn-about" href="projects.php#code_javascript">See more...</a></p>
+           </section>
+           <!-- End of Section 'Code and Projects' -->
