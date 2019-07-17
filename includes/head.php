1f8b30bf37a0dc2f15bfebb10723d8f7c40089de
FeliOdras <ariane.feldmann@odras.de>
2019-07-17 09:17:23 +0200

    Removed console logs

--- includes/head.php
+++ includes/head.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-    <title><?php echo $headTitle; ?></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+<title><?php echo $headTitle; ?></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
 </head>
